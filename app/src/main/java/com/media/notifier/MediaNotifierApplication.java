f525ab9d9bfa822b454935aaf1866556013b06d4
Dmytro Stepanyshchenko <mainalive@gmail.com>
2022-03-05 17:41:19 +0200

    Build jar + docker image

--- app/src/main/java/com/media/notifier/MediaNotifierApplication.java
+++ app/src/main/java/com/media/notifier/MediaNotifierApplication.java
@@ -1,7 +1,11 @@
 package com.media.notifier;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MediaNotifierApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaNotifierApplication.class, args);
+    }
 }
